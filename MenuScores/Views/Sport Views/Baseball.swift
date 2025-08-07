9b113697ed8725df6df8b3aa829a92ca1538e210
suhasvemuri <14100132+suhasvemuri@users.noreply.github.com>
2025-08-07 11:14:18 -0400

    Removed intial expand for baseball

--- MenuScores/Views/Sport Views/Baseball.swift
+++ MenuScores/Views/Sport Views/Baseball.swift
@@ -183,8 +183,8 @@ struct BaseballMenu: View {
 
                                 DynamicNotchManager.shared.currentNotch = notch
 
-                                await notch.expand()
-                                try? await Task.sleep(for: .seconds(2))
+//                                await notch.expand()
+//                                try? await Task.sleep(for: .seconds(2))
                                 await notch.compact()
                             }
                         } label: {
